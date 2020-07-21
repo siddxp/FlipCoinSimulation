8658286af68946b832766a51c0ca39c58db27c69
siddxp <siddhantk071@gmail.com>
2020-07-21 14:39:31 +0530

    UC--2-Count of heads and tails added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -x
-randomCoin=$(( RANDOM%2 ))
-if [ $randomCoin -eq 0 ]
-then
-	echo " It's Heads"
-else
-	echo " It's Tails"
-fi
+headsCount=0
+tailsCount=0
+valid=true
+while [ valid ]
+do
+	randomCoin=$(( RANDOM%2 ))
+	if [ $randomCoin -eq 0 ]
+	then
+		echo " It's Heads"
+		headsCount=$(($headsCount+1))
+	else
+		echo " It's Tails"
+		tailsCount=$(($tailsCount+1))
+	fi
+done
+echo "Heads : $headsCount"
+echo "Tails : $tailsCount"
