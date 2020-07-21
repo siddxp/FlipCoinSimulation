f7f5939ce4302a10e42ad034ab238ce45e4afe54
siddxp <siddhantk071@gmail.com>
2020-07-21 15:02:17 +0530

    UC--4-code added in function to repeat itself

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,35 +2,41 @@
 headsCount=0
 tailsCount=0
 valid=true
-while [ valid ]
-do
-	randomCoin=$(( RANDOM%2 ))
-	if [ $randomCoin -eq 0 ]
-	then
-		echo " It's Heads"
-		headsCount=$(($headsCount+1))
-	else
-		echo " It's Tails"
-		tailsCount=$(($tailsCount+1))
-	fi
 
-	if [ $headsCount -eq 21 ]
-	then
-		result=$(( $headsCount-$tailsCount ))
-		echo "Heads won,difference of result is : " $result
-		break;
-	elif [ $tailsCount -eq 21 ]
-	then
-		result= $(( $tailsCount-$headsCount ))
-		echo "Tails won,difference of result is : " $result
-		break;
-	elif [ $headsCount -eq 21 ] && [ $tailsCount -eq 21 ]
-	then
-		echo "It's a Tie"
-		break;
-	else
-		echo "Invalid Result"
-	fi
-done
+function flipCoinSimulator()
+{
+
+	while [ valid ]
+	do
+		randomCoin=$(( RANDOM%2 ))
+		if [ $randomCoin -eq 0 ]
+		then
+			echo " It's Heads"
+			headsCount=$(($headsCount+1))
+		else
+			echo " It's Tails"
+			tailsCount=$(($tailsCount+1))
+		fi
+
+		if [ $headsCount -eq 21 ]
+		then
+			result=$(( $headsCount-$tailsCount ))
+			echo "Heads won,difference of result is : " $result
+			break;
+		elif [ $tailsCount -eq 21 ]
+		then
+			result= $(( $tailsCount-$headsCount ))
+			echo "Tails won,difference of result is : " $result
+			break;
+		elif [ $headsCount -eq 21 ] && [ $tailsCount -eq 21 ]
+		then
+			echo "It's a Tie"
+			break;
+		else
+			echo "Invalid Result"
+		fi
+	done
 echo "Heads : $headsCount"
 echo "Tails : $tailsCount"
+}
+flipCoinSimulator
