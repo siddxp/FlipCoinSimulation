aeab65800554c69a4820ec6e68c2e1765e0c542d
siddxp <siddhantk071@gmail.com>
2020-07-21 14:55:42 +0530

    UC--3-Result of heads ot tails winning added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 headsCount=0
 tailsCount=0
 valid=true
@@ -12,6 +12,24 @@ do
 	else
 		echo " It's Tails"
 		tailsCount=$(($tailsCount+1))
+	fi
+
+	if [ $headsCount -eq 21 ]
+	then
+		result=$(( $headsCount-$tailsCount ))
+		echo "Heads won,difference of result is : " $result
+		break;
+	elif [ $tailsCount -eq 21 ]
+	then
+		result= $(( $tailsCount-$headsCount ))
+		echo "Tails won,difference of result is : " $result
+		break;
+	elif [ $headsCount -eq 21 ] && [ $tailsCount -eq 21 ]
+	then
+		echo "It's a Tie"
+		break;
+	else
+		echo "Invalid Result"
 	fi
 done
 echo "Heads : $headsCount"
